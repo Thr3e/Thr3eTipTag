1cb9023e091ac1fa967c2b8068ef8ba912650c1d
Thr3e <tony_thr3e@163.com>
2018-06-25 02:50:54 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
 (function(){
-    var a = new THRTipTag({
-        'msg' : 'test'
+    var tiptagObj = new THRTipTag('这里是提示内容');
+    
+    var btns = document.querySelectorAll('.test-btn');
+    btns.forEach((val, idx) => {
+        var curIdx = idx;
+        btns[idx].onclick = function(){
+            if (curIdx === 0) {
+                tiptagObj.showTip();
+            } else if (curIdx === 1) {
+                tiptagObj.showTipWithConfBtn((obj) => { 
+                    tiptagObj = obj;
+                    if (tiptagObj.status !== 'error'){
+                        var altStr = tiptagObj.status === 'cancle' ? '取消' : '确认';
+                        alert('你点击了' + altStr + '键');
+                    }
+                })
+            } else if (curIdx === 2) {
+                tiptagObj.showTipWithCancleBtn((obj) => { 
+                    tiptagObj = obj;
+                    if (tiptagObj.status !== 'error'){
+                        var altStr = tiptagObj.status === 'cancle' ? '取消' : '确认';
+                        alert('你点击了' + altStr + '键');
+                    }
+                })
+            } else if (curIdx === 3){
+                tiptagObj.showTipWithInput((obj) => { 
+                    tiptagObj = obj;
+                    if (tiptagObj.status !== 'error'){
+                        var altStr = tiptagObj.status === 'cancle' ? '取消键' : '确认键,输入的内容为：' + tiptagObj.val;
+                        alert('你点击了' + altStr);
+                    }
+                })
+            }
+        };
     })
 
-    a.showTip();
 })();
--- plugins/js/THRTipTag.js
+++ plugins/js/THRTipTag.js
@@ -1,68 +1,139 @@
-function THRTipTag(obj){
-    this.msg = obj.msg,
-    this.bgc = obj.bgc,
-    this.btnColor = obj.btnColor
+function THRTipTag(msg){
+    this.msg = msg,
+    this.type = '',
+    this.status = '',
+    this.value = '',
+    this.placeholder = '请输入...',
+    this.styleFamily = 'red',
+    this.style = {
+        'red' : { 'background' : '#FBFBFB', 'border' : '#B9E1DC', 'btn' : '#F38181', 'color' : '#756C83'},
+        'bule' : { 'background' : '#DBEDF3', 'border' : '#00818A', 'btn' : '#404B69', 'color' : '#283149'},
+        'green' : { 'background' : '#F4F9F4', 'border' : '#C4E3CB', 'btn' : '#8AAE92', 'color' : '#616161'},
+        'gray' : { 'background' : '#F7F7F7', 'border' : '#93DEFF', 'btn' : '#606470', 'color' : '#323643'}
+    }
 }
 
-THRTipTag.prototype.showTip = THRTipTag.prototype.showTip || function(){
+THRTipTag.prototype.showTip = function(){
     this.type = 'tips';
-    createTag(this.msg, this.type);
+    var _self = this;
+    createTag(_self, function(status) { 
+        _self.status = status;
+    });
 }
 
-THRTipTag.prototype.showTipWithConfBtn = THRTipTag.prototype.showTipWithConfBtn || function(){
-    this.type = 'confTips'
-    createTag(this.msg, this.type);
+THRTipTag.prototype.showTipWithConfBtn = function(callback){
+    this.type = 'confTips';
+    var _self = this;
+    createTag(_self, function(status) { 
+        _self.status = status;
+        callback(_self);
+    });
 }
 
-THRTipTag.prototype.show = function(){
-    
+THRTipTag.prototype.showTipWithCancleBtn = function(callback){
+    this.type = 'cancleTips';
+    var _self = this;
+    createTag(_self, function(status) { 
+        _self.status = status;
+        callback(_self);
+    });
 }
 
-THRTipTag.prototype.show = function(){
-    
-}
-
-THRTipTag.prototype.show = function(){
-    
+THRTipTag.prototype.showTipWithInput = function(callback){
+    this.type = 'inputTips';
+    var _self = this;
+    createTag(_self, function(status, value) { 
+        _self.status = status;
+        _self.val = value;
+        callback(_self);
+    });
 }
 
 function getCreateElement(content, parent, idStr, docType){
     var new_doc = document.createElement(docType || 'div');
-    new_doc.textContent = content;
     parent.appendChild(new_doc);
     new_doc.id = 'thr3e-' + idStr;
+    if (docType === 'input') {
+        new_doc.setAttribute('type', 'text');
+        new_doc.setAttribute('placeholder', content);
+    } else {
+        new_doc.textContent = content;
+    }
     return new_doc;
 }
 
-function createTag (msg, type){
-    if (!msg || !type){
+function createTag (obj, callback){
+    if (!obj.msg || !obj.type){
         throw('this tag must have message and type!');
         return;
     }
 
     var backMask = getCreateElement('', document.body, 'mask'),
         tipView  = getCreateElement('', backMask, 'tag-view'),
-        Content  = getCreateElement(msg, tipView, 'content', 'p')
-        btnWrap  = getCreateElement('', tipView, 'btn-wrap')
-        confBtn  = getCreateElement('确认', btnWrap, 'conf-btn'),
+        Content  = getCreateElement(obj.msg, tipView, 'content', 'p'),
+        userInput= getCreateElement(obj.placeholder, tipView, 'input', 'input'),        
+        btnWrap  = getCreateElement('', tipView, 'btn-wrap'),
         cancleBtn= getCreateElement('取消', btnWrap, 'cancle-btn'),
-        userInput= document.createElement('input');
-    userInput.setAttribute('type', 'text');
-    userInput.id = 'thr3e-input'
-    tipView.appendChild(userInput);
-    
-    if (type === 'tips'){
+        confBtn  = getCreateElement('确认', btnWrap, 'conf-btn');
+
+    setStyle(obj, {'tipView' : tipView, 'confBtn' : confBtn, 'btnWrap' : btnWrap, 'userInput' : userInput});
+
+    confBtn.classList.add('thr3e-hide');
+    cancleBtn.classList.add('thr3e-hide');
+    userInput.classList.add('thr3e-hide');
+
+    cancleBtn.onclick = function() {
+        if (removeMask() === 'seccuse') callback('cancle');
+        else callback('error');
+    }
+    confBtn.onclick = function(){
+        var val = '';
+        if (!userInput.classList.contains('thr3e-hide')){
+            if (userInput.value){
+                val = userInput.value;
+            } else {
+                userInput.classList.add('thr3e-uninput');
+                userInput.setAttribute('placeholder', '请输入内容');
+                userInput.onfocus = function(){
+                    userInput.classList.remove('thr3e-uninput');
+                }
+                return;
+            }
+        };
+
+        if (removeMask() === 'seccuse') callback('confirm', val);
+        else callback('error', 'NO-MESSAGE');
+    }
+
+    if (obj.type === 'tips'){
         tipView.className = 'tip';
         backMask.style.background = "#fff0";
     }else{
-        var elArr = [confBtn, type !== 'confTips' ? cancleBtn : '', type === 'inputTips' ? userInput : ''];
+        var elArr = [confBtn, obj.type !== 'confTips' ? cancleBtn : '', obj.type === 'inputTips' ? userInput : ''];
         elArr.forEach ((val) => {
-            if (val) val.style.display = 'block';
+            if (val) val.classList.remove('thr3e-hide');
         })
     }
-    
-
-    
+}
 
 
+
+function removeMask(){
+    var mask = document.body.querySelector('#thr3e-mask');
+    if (mask) {
+        mask.remove();
+        return 'seccuse';
+    } 
+
+    throw('ERROR : error_0');
+    return 'error'
+}
+
+
+function setStyle(obj, elements) {
+    var style = obj.style[obj.styleFamily]
+    elements['tipView'].style.cssText = `background : ${style.background}; color : ${style.color}; border-color : ${style.border}`;
+    elements['confBtn'].style.cssText = `background : ${style.btn}; color : ${style.background}`;
+    elements['userInput'].style.cssText = 'border-color : ' + style.border;
+    elements['btnWrap'].style.cssText = 'border-color : ' + style.border;
 }
