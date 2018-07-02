de6e0ad2a9cf3798c58c4c67101c28c313e1c372
Thr3e <tony_thr3e@163.com>
2018-07-03 01:51:01 +0800

    update

--- js/index.js
+++ js/index.js
@@ -46,7 +46,7 @@
             type: "prompt",
             title: "验证提示",
             // highlightColor:"#52d681",
-            iconURL:"./plugin/imgs/message.svg",
+            iconURL:"/plugin/imgs/message.svg",
             placeholder: "请输入您的身份证号！",
             confCallBack: function (text) {
                 console.log(text);
